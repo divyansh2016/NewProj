533481d134dba6ed00d885fa44a413b27141c8d9
divyansh2016 <70529963+divyansh2016@users.noreply.github.com>
2020-09-02 14:53:52 +0530

    Update Exit.java

--- Exit.java
+++ Exit.java
@@ -1,8 +1,10 @@
 class Exit{
 public static void main(String args[]){
-
+int a;
+  Sysyem.out.println("Hello Web Application");
 System.exit();
-Sysyem.out.println("Hello Web Application");
+  Sysyem.out.println("a value is"+a);
+
 
 }
 
