732a5d4867b29b4110098b86ffb12b7b008347b7
mani <mani@gmail.com>
2020-09-06 11:14:50 +0530

    Add.java modified

--- Add.java
+++ Add.java
@@ -1,8 +1,8 @@
 class Add{
 public static void main(String args[]){
-
+System.out.println("Addioin of a,b and c is :");
 int a=5,b=10,c=80;
-System.out.println("Addition of a,b and cis:"(a+b+c));
+System.out.println(a+b+c);
 
 }
 }
