6f67ffa2e0d28c1da5afe81bbeb8eb35260adbd2
divyansh2016 <70529963+divyansh2016@users.noreply.github.com>
2020-09-06 10:33:08 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -3,9 +3,9 @@ class EvenOdd{
 public static void main(String args[]){
 
 
-Scanner sc=new Scanner(System.in)
+Scanner sc=new Scanner(System.in);
 int num=sc.nextInt();
-if(num%==0)
+if(num%2==0)
 System.out.println("The given number is Even");
 else
 System.out.println("The given number is odd");
